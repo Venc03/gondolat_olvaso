365935d83c91eacc347d7cf57ec682a51295d6d9
Végi Dániel Márk <vegi.d.mark@dk.akkszalezi.hu>
2023-09-11 14:58:11 +0200

    keveres elkezdve, kommittal ellatva a hianyos es hibas reszek

--- src/gondolatolvaso/Main.java
+++ src/gondolatolvaso/Main.java
@@ -4,44 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    static String[] pakli = new String[24];
+
     public static void main(String[] args) {
-        
+
         Kirak();
         Melyik();
         Kever();
         EzVolt();
     }
-        
+
     public static void Kirak() {
-        
-    String[] pakli = new String[24];
-                    
-    String[] szinek = {"P", "M", "Z", "T"};
-                    
-    String[] ertekek = {"Ász","Kir","Fel","X","IX", "VIII"};
-    
-    
-    int i = 1;
-    int lap = 0;
-    
-    
+
+        String[] szinek = {"P", "M", "Z", "T"};
+
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+
+        int i = 1;
+        int lap = 0;
+
         for (int j = 0; j < ertekek.length; j++) {
             for (int k = 0; k < szinek.length; k++) {
                 pakli[lap] = szinek[k] + "_" + ertekek[j];
-                lap ++;
+                lap++;
             }
-            
+
         }
-  
-          for (int j = 0; j < pakli.length; j++) {
-              System.out.println(pakli[j]);
-            
+
+        for (int j = 0; j < pakli.length; j++) {
+            System.out.println(pakli[j]);
+
         }
-   
-                
-        
-        
-    
+
     }
 
     public static void Melyik() {
@@ -57,43 +51,50 @@ public class Main {
     }
 
     public static void Kever() {
-            switch(1){
-                case 1:
-                    for (i < 7){
-                        [i] = 20-(i-1)*3;
+        //switch 1, 2, 3 nincs kesz
+        //----------------------------------------
+        switch (1) {
+            case 1:
+                for (int i = 1; i < 7; i++) {
+                    [i] = 20 - (i - 1)*3;
+                }
                         break;
-                    }
-                case 2:
-                    [i+7] = 19-(i-1)*3;
+            case 2:
+                [i + 7] = 19-(i - 1)*3;
                     break;
-                case 3:
-                    [i+14] = 21-(i-1)*3;
-                    break;  
-            }
-            
-            switch(2){
-                case 1:
-                    [i] = 19-(i-1)*3;
-                    break;
-                case 2:
-                    [i+7] = 20-(i-1)*3;
-                    break;
-                case 3:
-                    [i+14] = 21-(i-1)*3;
-                    break;  
-            }
-            
-            int oszlop = 2;
-            switch(oszlop) { 
             case 3:
-                   
-            }
-    } 
-                
-    public static void EzVolt() {
-        
-                    
-    }
-        
-}
+                [i + 14] = 21-(i - 1)*3;
+                    break;
+        }
 
+        switch (2) {
+            case 1:
+                [i
+                ] = 19-(i - 1
+                )*3;
+                    break;
+            case 2:
+                [i + 7
+                ] = 20-(i - 1
+                )*3;
+                    break;
+            case 3:
+                [i + 14
+                ] = 21-(i - 1
+                )*3;
+                    break;
+        }
+
+        int oszlop = 2;
+        switch (oszlop) {
+            case 3:
+
+        }
+        //----------------------------------------
+    }
+
+    public static void EzVolt() {
+        //ures, kell egy kiiro kod
+    }
+
+}
