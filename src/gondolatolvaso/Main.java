1c1deb5a77a4a768090521da0dffe498266e61ca
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-09-12 23:03:53 +0200

    kesz

--- src/gondolatolvaso/Main.java
+++ src/gondolatolvaso/Main.java
@@ -131,7 +131,7 @@ public class Main {
         int index = 0;
         int sor = 0;
         while (index < pakli.length) {
-            System.out.printf("%9s", pakli[kevertPakliIndex[index]] + 1);
+            System.out.printf("%9s", pakli[kevertPakliIndex[index]]);
             index++;
             if (index % 3 == 0) {
                 System.out.print(" |");
