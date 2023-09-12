89d12750d0b112980f40789c738aa5bd441e7fe0
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-09-12 21:18:48 +0200

    pakli feltoltes javitas, case 3 majdnek kesz

--- src/gondolatolvaso/Main.java
+++ src/gondolatolvaso/Main.java
@@ -4,37 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String[] pakli = new String[24];
+    static String[] pakli = new String[21];
 
     public static void main(String[] args) {
 
         Kirak();
-        int hanyadik = Melyik();
-        Kever(hanyadik);
+        int oszlopId = Melyik();
+        Kever(oszlopId);
         EzVolt();
     }
 
     public static void Kirak() {
 
         String[] szinek = {"P", "M", "Z", "T"};
-
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
 
-        int i = 1;
+        int i = 0;
         int lap = 0;
-
-        for (int j = 0; j < ertekek.length; j++) {
-            for (int k = 0; k < szinek.length; k++) {
-                pakli[lap] = szinek[k] + "_" + ertekek[j];
-                lap++;
-            }
-
+        for (String szin: szinek){
+          for (int j = 0; i < 21 && j < ertekek.length; j++) {
+           pakli[i] = szin + "_" + ertekek[j];
+           i += 1;
+          
+            
         }
+        
+        }
+        
+      
+
+     
 
         for (int j = 0; j < pakli.length; j++) {
 
             System.out.println(pakli[j] + "   " + j);
-         
 
         }
 
@@ -42,26 +45,24 @@ public class Main {
 
     public static int Melyik() {
 
-   
-
         Scanner src = new Scanner(System.in);
         boolean jo;
+        int oszlop = 0;
         do {
             System.out.println("oszlop(1-3): ");
-            int oszlop = src.nextInt();
+            oszlop = src.nextInt();
             jo = oszlop >= 1 && oszlop <= 3;
         } while (!jo);
 
-        return 3;
+        return oszlop;
 
     }
 
-    public static void Kever(int hanyadik) {
-        
-        
-    //switch 1, 2, 3 nincs kesz
+    public static void Kever(int oszlopId) {
+
+        //switch 1, 2, 3 nincs kesz
         //----------------------------------------
-        switch (1) {
+        /*    switch (1) {
             case 1:
                 for (int i = 1; i < 7; i++) {
                     [i] = 20 - (i - 1)*3;
@@ -99,27 +100,40 @@ public class Main {
 
         }
     
+         */
         
-        String valasztott = "";
-        
-        switch (hanyadik) {
+
+        switch (oszlopId) {
+            case 1:
+                break;
+            case 2:
+                   break;
             case 3:
-                hanyadik = 21;
-                
-                    for (int i = 1; i < 7; i++) {
-                        valasztott = pakli[hanyadik];
-                        pakli[i] = valasztott;       
-                        System.out.println(valasztott);
-                        hanyadik += -3;
-                    }
-                }
-             
-        
+                keveres3(); 
+                break;
+        }
 
     }
 
     public static void EzVolt() {
         //ures, kell egy kiiro kod
 
-}
+    }
+
+    private static void keveres3() {
+        String [] kevertPakli = new String[21];
+        int kartyaIndex = 21;
+        String valasztott = "";
+         for (int i = 0; i < kevertPakli.length; i++) {
+                    valasztott = pakli[kartyaIndex];
+                    kevertPakli[i] = kartyaIndex;
+                    System.out.print(valasztott);
+                    System.out.println(" "+kartyaIndex);
+                    kartyaIndex += -3;
+                    if(kartyaIndex <= 0){
+                        kartyaIndex += 20;
+                    }
+                }
+    }
+;
 }
